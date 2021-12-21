7b824bd1aeea0ab59cfb3fece08ab173f55ba5a2
Rafael <rafael.gcaldas01@gmail.com>
2021-12-20 23:41:32 -0300

    Search products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,40 @@
 import { NextPage } from "next"
+import { FormEvent, useState } from "react"
+import { SearchResults } from "../components/SearchResults";
 
 const Home: NextPage = () => {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input 
+          type="text" 
+          value={search} 
+          onChange={e => 
+          setSearch(e.target.value)} 
+        />
+        <button type="submit">Buscar</button>
+      </form>
+      
+      <SearchResults results={results} />
+    </div>
   )
 }
 
