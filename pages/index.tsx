8556fdba600223dbb1fc2371d36933f32df6c69e
Rafael <rafael.gcaldas01@gmail.com>
2022-01-05 22:56:23 -0300

    useCallback

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next"
-import { FormEvent, useState } from "react"
+import { FormEvent, useCallback, useState } from "react"
 import { SearchResults } from "../components/SearchResults";
 
 const Home: NextPage = () => {
@@ -19,6 +19,10 @@ const Home: NextPage = () => {
     setResults(data);
   }
 
+  const addToWishList = useCallback(async (id: number) => {
+    console.log(id);
+  }, [])
+
   return (
     <div>
       <h1>Search</h1>
@@ -33,7 +37,10 @@ const Home: NextPage = () => {
         <button type="submit">Buscar</button>
       </form>
       
-      <SearchResults results={results} />
+      <SearchResults 
+        results={results}
+        onAddToWishList={addToWishList}
+      />
     </div>
   )
 }
